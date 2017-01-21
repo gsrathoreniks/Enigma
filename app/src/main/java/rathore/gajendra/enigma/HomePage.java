a0996af5ad1278b7bf4905c6840f8608aab75aee
Avinash_Singh <avi13smartest@gmail.com>
2017-01-22 02:02:02 +0530

    Added Map to the drawer layout..

--- app/src/main/java/rathore/gajendra/enigma/HomePage.java
+++ app/src/main/java/rathore/gajendra/enigma/HomePage.java
@@ -1,5 +1,6 @@
 package rathore.gajendra.enigma;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,17 +81,15 @@ public class HomePage extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_map) {
+            // Handles the Location of the user.
+            Intent d = new Intent(this, map.class);
+            startActivity(d);
 
-        } else if (id == R.id.nav_slideshow) {
+            if(id== R.id.nav_profile){
 
-        } else if (id == R.id.nav_manage) {
+            }
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
